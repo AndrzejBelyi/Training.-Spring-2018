40d0be3d97d19f31e0eb7d03d42b955cea5adaf3
Andrzej <Andrzej.Belyi@gmail.com>
2018-03-14 20:13:23 +0300

    Added a partial solution
    Added a partial solution for my task.
    I wont to see what i will have after upload.

---
+++ Day 1/NET.S.2018.Belyi.01/ArraySorts.Tests/SortsTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArraySorts.Tests
+{
+    [TestClass]
+    public class SortsTests
+    {
+        [TestMethod]
+        public void QuickSort_4231_1234()
+        {
+            int[] arr1 = new int[4] {4, 2, 3, 1};
+            ArraySorts.Sorts.QuickSort(arr1);
+            CollectionAssert.AreEqual(new int[4] { 1, 2, 3, 4},arr1);
+        }
+        [TestMethod]
+        public void MergeSort_4231_1234()
+        {
+            int[] arr1 = new int[4] {4,2,3,1};
+            arr1 = ArraySorts.Sorts.MergeSort(arr1);
+            CollectionAssert.AreEqual(new int[4] { 1, 2, 3, 4 }, arr1);
+            
+        }
+    }
+}
---
+++ Day 1/NET.S.2018.Belyi.01/ArraySorts/Sorts.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySorts
+{
+    /// <summary>
+    /// This static class contains static methods to sort(quick sort,merge sort)
+    /// </summary>
+    public static class Sorts
+    {
+        /// <summary>
+        /// This public method used to calling overloaded version of QuickSort
+        /// </summary>
+        /// <param name="array"></param>
+        public static void QuickSort(int[] array)
+        {
+            if (array != null && array.Length > 1)
+            {
+                QuickSort(array, 0, array.Length - 1);
+            }
+        }       
+
+        /// <summary>
+        /// This public method used to calling overloaded version of QuickSort
+        /// </summary>
+        /// <param name="array"></param>
+        public static int[] MergeSort(int[] array)
+        {
+            if (array.Length < 2)
+            {
+                return array;
+            }
+            else
+            {
+                int middle = array.Length / 2;
+                int[] left = array.Take(middle).ToArray();
+                int[] right = array.Skip(middle).ToArray();
+                return Merge(MergeSort(left),MergeSort(right));
+            }
+        }
+
+        /// <summary>
+        /// This private recursive method used to sort array using quick sort algorithm 
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="first"></param>
+        /// <param name="last"></param>
+        private static void QuickSort(int[] array, int first, int last)
+        {
+            int pivotElement = array[((last - first) / 2) + first];
+            int i = first, j = last;
+            while (i <= j)
+            {
+                while (array[i] < pivotElement && i <= last)
+                {
+                    i++;
+                }
+
+                while (array[j] > pivotElement && j >= first)
+                {
+                    j--;
+                }
+
+                if (i <= j)
+                {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+
+            if (j > first)
+            {
+                QuickSort(array, first, j);
+            }
+
+            if (i < last)
+            {
+                QuickSort(array, i, last);
+            }
+        }
+
+        /// <summary>
+        /// This private method sorting array using merge sort algorithm
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns>return merged array</returns>
+        private static int[] Merge(int[] left, int[] right)
+        {
+            int j = 0, k = 0;
+            int[] merged = new int[left.Length + right.Length]; 
+            for (int i = 0; i < merged.Length; i++)
+            {
+                if (j < left.Length && k < right.Length)
+                {
+                    if (left[j] < right[k])
+                    {
+                        merged[i] = left[j];
+                        j++;
+                    }
+                    else
+                    {
+                        merged[i] = right[k];
+                        k++;
+                    }
+                }
+                else
+                {
+                    if (k < right.Length)
+                    {
+                        merged[i] = right[k];
+                        k++;
+                    }
+                    else
+                    {
+                        merged[i] = left[j];
+                        j++;
+                    }
+                }
+            }
+
+            return merged;
+        }
+    }
+}
