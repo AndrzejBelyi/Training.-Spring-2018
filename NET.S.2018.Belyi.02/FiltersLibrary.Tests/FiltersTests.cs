ac6d7a6972de2c4bfc788bc2496d13c0c323169f
Andrzej <Andrzej.Belyi@gmail.com>
2018-03-17 18:41:12 +0300

    Added perfomance tests

--- NET.S.2018.Belyi.02/FiltersLibrary.Tests/FiltersTests.cs
+++ NET.S.2018.Belyi.02/FiltersLibrary.Tests/FiltersTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace FiltersLibrary.Tests
@@ -56,6 +58,66 @@ namespace FiltersLibrary.Tests
             int[] expectedArray = new int[4] { 7, 7, 70, 17 };
             actualArray = Filters.FilterDigit(actualArray, 11);
             CollectionAssert.AreEqual(expectedArray, actualArray);
-        }       
+        }
+
+        [TestMethod]
+        public void DigitFilter_Perfomance()
+        {
+            int[] array = Enumerable.Range(1,7000000).ToArray();
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            Filters.FilterDigit(array, 7);
+            stopWatch.Stop();
+            Console.WriteLine("Время сортировки 10000000 элементов: " + stopWatch.Elapsed);
+
+        }
+
+        [TestMethod]
+        public void DigitFilter0_Perfomance()
+        {
+            int[] array = Enumerable.Range(1, 7000000).ToArray();
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            Filters.FilterDigit0(array, 7);
+            stopWatch.Stop();
+            Console.WriteLine("Время сортировки 10000000 элементов: " + stopWatch.Elapsed);
+
+        }
+
+        [TestMethod]
+        public void DigitFilter1_Perfomance()
+        {
+            int[] array = Enumerable.Range(1, 7000000).ToArray();
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            Filters.FilterDigit1(array, 7);
+            stopWatch.Stop();
+            Console.WriteLine("Время сортировки 10000000 элементов: " + stopWatch.Elapsed);
+
+        }
+
+        [TestMethod]
+        public void DigitFilter2_Perfomance()
+        {
+            int[] array = Enumerable.Range(1, 7000000).ToArray();
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            Filters.FilterDigit2(array, 7);
+            stopWatch.Stop();
+            Console.WriteLine("Время сортировки 10000000 элементов: " + stopWatch.Elapsed);
+
+        }
+
+        [TestMethod]
+        public void DigitFilter3_Perfomance()
+        {
+            int[] array = Enumerable.Range(1, 7000000).ToArray();
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            Filters.FilterDigit3(array, 7);
+            stopWatch.Stop();
+            Console.WriteLine("Время сортировки 10000000 элементов: " + stopWatch.Elapsed);
+
+        }
     }
 }
--- NET.S.2018.Belyi.02/FiltersLibrary/Filters.cs
+++ NET.S.2018.Belyi.02/FiltersLibrary/Filters.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,8 +15,8 @@ namespace FiltersLibrary
         /// <summary>
         /// This method returns an array of numbers that contain a given digit
         /// </summary>
-        /// <param name="array"></param>
-        /// <param name="targetDigit"></param>
+        /// <param name="array">Array for filtration</param>
+        /// <param name="targetDigit">digit for filtration</param>
         /// <returns>Filtered array</returns>
         public static int[] FilterDigit(int[] array, int targetDigit)
         {
@@ -41,6 +42,134 @@ namespace FiltersLibrary
 
                 return filtredArray.ToArray();
             }
+        }
+
+        public static ArrayList FilterDigit0(int[] listInts, int number)
+        {
+            if (listInts.Length == 0)
+            {
+                throw new ArgumentException();
+            }
+
+            ArrayList listFilter = new ArrayList();
+            for (int i = 0; i < listInts.Length; i++)
+            {
+                string supportString = listInts[i].ToString();
+                string numberString = number.ToString();
+                if (supportString.Contains(numberString))
+                {
+                    listFilter.Add(listInts[i]);
+                }
+            }
+
+            return listFilter;
+        }
+
+        public static int[] FilterDigit1(int[] numbers, int digit)
+        {
+            if ((digit < 0) || (digit > 9))
+            {
+                throw new ArgumentException($"{nameof(digit)} must be from 0 to 9", nameof(digit));
+            }
+
+            if (numbers == null)
+            {
+                throw new ArgumentException(nameof(numbers));
+            }
+
+            if (numbers.Length == 0)
+            {
+                return null;
+            }
+
+            int[] result = new int[0];
+            string digitStr = digit.ToString();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                string number = numbers[i].ToString();
+                if (number.Contains(digitStr))
+                {
+                    Array.Resize(ref result, result.Length + 1);
+                    result[result.Length - 1] = numbers[i];
+                }
+            }
+
+            return result;
+        }
+
+        public static int[] FilterDigit2(int[] numbers, int digit)
+        {
+            if ((digit < 0) || (digit > 9))
+            {
+                throw new ArgumentException($" {nameof(digit)} must be from 0 to 9", nameof(digit));
+            }
+
+            if (numbers == null)
+            {
+                throw new ArgumentException(nameof(numbers));
+            }
+
+            if (numbers.Length == 0)
+            {
+                return null;
+            }
+
+            List<int> result = new List<int>();
+            string digitStr = digit.ToString();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                string number = numbers[i].ToString();
+                if (number.Contains(digitStr))
+                {
+                    result.Add(numbers[i]);
+                }
+            }
+
+            return result.ToArray();
+        }
+
+        public static int[] FilterDigit3(int[] numbers, int digit)
+        {
+            if ((digit < 0) || (digit > 9))
+            {
+                throw new ArgumentException($"{nameof(digit)} must be from 0 to 9", nameof(digit));
+            }
+
+            if (numbers == null)
+            {
+                throw new ArgumentException(nameof(numbers));
+            }
+
+            if (numbers.Length == 0)
+            {
+                throw new ArgumentException(nameof(numbers));
+            }
+
+            List<int> result = new List<int>();
+            foreach (var element in numbers)
+            {
+                if (IsContainsDigit(element, digit))
+                {
+                    result.Add(element);
+                }
+            }
+
+            return result.ToArray();
+        }
+
+        private static bool IsContainsDigit(int number, int digit)
+        {
+            while (number != 0)
+            {
+                if (number % 10 == digit)
+                {
+                    return true;
+                }
+
+                number = number / 10;
+            }
+
+            return false;
         }
     }
 }
