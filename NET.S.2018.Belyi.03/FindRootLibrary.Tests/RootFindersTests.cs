b6a151178c1aaf2fc1854118468dafdbec655273
Andrzej <Andrzej.Belyi@gmail.com>
2018-03-20 17:08:47 +0300

    Added FindNthRoot(some tests are failed)

---
+++ NET.S.2018.Belyi.03/FindRootLibrary.Tests/RootFindersTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FindRootLibrary;
+namespace FindRootLibrary.Tests
+{
+    [TestFixture]
+    public class RootFindersTests
+    {
+        [Test]
+        [TestCase(1, 5, 0.1, 1)]
+        [TestCase(8, 1, 0.1, 8)]
+        [TestCase(1, 6, 0.1, 1)]
+        //[TestCase(0.001, 3, 0.1, 0.1)]
+        //[TestCase(0.04100625, 4, 0.01, 0.45)]
+        //[TestCase(8, 3, 0.0001, 2)]
+        //[TestCase(0.0279936, 7, 0.0001, 0.6)]
+        //[TestCase(0.0081, 4, 0.1, 0.3)]
+        //[TestCase(-0.008, 3, 0.1, -0.2)]
+        //[TestCase(0.004241979, 9, 0.00000001, 0.545)]
+        public void FindNthRoot_ValidDataTest(double number, int rank, double precision, double expected)
+        {          
+            Assert.AreEqual(expected, RootFinders.FindNthRoot(number, rank, precision));
+        }
+
+        [Test]
+        [TestCase(8, 15, -7, -5)]
+        [TestCase(8, 15, -0.6, -0.1)]
+        public void FindNthRoot_Degree_Precision_ArgumentOutOfRangeException(double number, int rank, double eps, double expected)
+            => Assert.Throws<ArgumentOutOfRangeException>(() => RootFinders.FindNthRoot(number, rank, eps));
+    }
+}
---
+++ NET.S.2018.Belyi.03/FindRootLibrary/RootFinders.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindRootLibrary
+{
+   public static class RootFinders
+    {
+        /// <summary>
+        /// Find a n-root of number
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="rank"></param>
+        /// <param name="eps"></param>
+        /// <returns>Retruns root</returns>
+        public static double FindNthRoot(double number,int rank, double eps)
+        {
+            if(rank < 0 || number < 0 || eps <= 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if(rank == 1 || number == 1)
+            {
+                return number;
+            }            
+            double root = ((rank - 1) + number) / rank;
+            double x = 1;
+            while (Math.Abs(root - x) > eps)
+            {
+                x = root;
+                root = (((rank - 1) * x) + (number / Math.Pow(x, rank - 1))) / rank;
+            }
+
+            return root;
+        }
+    }
+}
