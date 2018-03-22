1509ea335b132b5f0bac6c2e4e6c5e991c77a27c
Andrzej <Andrzej.Belyi@gmail.com>
2018-03-22 17:29:10 +0300

    Added task1
    Возможны проблемы со стилем,моя VS падает при проверке StyleCop-ом.

---
+++ NET.S.2018.Belyi.05/Task1Library.Tests/GCDTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using static Task1Library.GCD;
+
+namespace Task1Library.Tests
+{
+    public class GCDTests
+    {
+        [Test]
+        [TestCase(9, -3,3)]
+        [TestCase(10000, 15000,5000)]
+        [TestCase(10, 10, 10)]
+        [TestCase(10, 9, 1)]
+        [TestCase(-3, 9, 3)]
+        [TestCase(15000, 10000, 5000)]
+        [TestCase(10, 10, 10)]
+        [TestCase(9, 10, 1)]
+        public void GetGcdByEuclid_TwoArguments(int firstNumber, int secondNumber, int expectedResult)
+        {
+            int actualResult = GCD.GetGCDByEuclid(firstNumber, secondNumber);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        [TestCase(-9,-18,27,9)]
+        [TestCase(10,10,9,1)]
+        [TestCase(3,3,3,3)]
+        [TestCase(0,3,6,3)]
+        [TestCase(0, 0, 3, 3)]
+        public void GetGcdByEuclid_ThreeArguments(int firstNumber, int secondNumber,int thirdNumber, int expectedResult)
+        {
+            int actualResult = GCD.GetGCDByEuclid(firstNumber, secondNumber,thirdNumber);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        [TestCase(9, -18, 27,-36, 9)]
+        [TestCase(1,9,7,5,3,11)]
+        [TestCase(3, 3, 3, 3, 3, 3, 3, 3)]
+        [TestCase(3, 3, 6, 9, 0)]
+        [TestCase(3, 0, 6, 3, 0)]
+        public void GetGcdByEuclid_ManyArguments(int expectedResult,params int[] arguments)
+        {
+            int actualResult = GCD.GetGCDByEuclid(arguments);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void GetGcdByEuclid_NullArgument_NullArgumentException()
+            => Assert.Throws<ArgumentNullException>(() => GCD.GetGCDByEuclid(null));
+
+        [Test]
+        public void GetGcdByEuclid_OneArgument_ArgumentException()
+            => Assert.Throws<ArgumentException>(() => GCD.GetGCDByEuclid(12));
+
+
+        [Test]
+        [TestCase(9, -3, 3)]
+        [TestCase(10000, 15000, 5000)]
+        [TestCase(10, 10, 10)]
+        [TestCase(10, 9, 1)]
+        [TestCase(-3, 9, 3)]
+        [TestCase(15000, 10000, 5000)]
+        [TestCase(10, 10, 10)]
+        [TestCase(9, 10, 1)]
+        public void GetGcdByStein_TwoArguments(int firstNumber, int secondNumber, int expectedResult)
+        {
+            int actualResult = GCD.GetGCDByStein(firstNumber, secondNumber);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        [TestCase(-9, -18, 27, 9)]
+        [TestCase(10, 10, 9, 1)]
+        [TestCase(3, 3, 3, 3)]
+        [TestCase(0, 3, 6, 3)]
+        public void GetGcdByStein_ThreeArguments(int firstNumber, int secondNumber, int thirdNumber, int expectedResult)
+        {
+            int actualResult = GCD.GetGCDByStein(firstNumber, secondNumber, thirdNumber);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        [TestCase(9, -18, 27, -36, 9)]
+        [TestCase(1, 9, 7, 5, 3, 11)]
+        [TestCase(3, 3, 3, 3, 3, 3, 3, 3)]
+        [TestCase(3, 3, 6, 9, 0)]
+        [TestCase(3, 0, 6, 3, 0)]
+        public void GetGcdByStein_ManyArguments(int expectedResult, params int[] arguments)
+        {
+            int actualResult = GCD.GetGCDByStein(arguments);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void GetGcdByStein_NullArgument_NullArgumentException()
+            => Assert.Throws<ArgumentNullException>(() => GCD.GetGCDByStein(null));
+
+        [Test]
+        public void GetGcdByStein_OneArgument_ArgumentException()
+            => Assert.Throws<ArgumentException>(() => GCD.GetGCDByStein(12));
+    }
+}
---
+++ NET.S.2018.Belyi.05/Task1Library/GCD.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1Library
+{
+    public static class GCD
+    {
+        #region EuclidRealization
+        /// <summary>
+        /// Finds and returns the greatest common divisor of two integers using the Euclidean algorithm
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <returns>Returns greater common divisor</returns>
+        public static int GetGCDByEuclid(int firstNumber, int secondNumber)
+        {
+            firstNumber = Math.Abs(firstNumber);
+            secondNumber = Math.Abs(secondNumber);
+            if (firstNumber == 0)
+            {
+                return secondNumber;
+            }
+
+            if (secondNumber == 0)
+            {
+                return firstNumber;
+            }
+
+            if (firstNumber == secondNumber)
+            {
+                return firstNumber;
+            }
+
+            if (firstNumber == 1 || secondNumber == 1)
+            {
+                return 1;
+            }
+
+            return GetGCDByEuclid(secondNumber, firstNumber % secondNumber);
+        }
+
+        /// <summary>
+        /// Finds and returns the greatest common divisor of three integers using the Euclidean algorithm
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <param name="thirdNumber"></param>
+        /// <returns>Returns greater common divisor</returns>
+        public static int GetGCDByEuclid(int firstNumber, int secondNumber, int thirdNumber)
+        {
+           return GetGCDByEuclid(GetGCDByEuclid(firstNumber, secondNumber), thirdNumber);
+        }
+
+        /// <summary>
+        /// Finds and returns the greatest common divisor of n integers using the Euclidean algorithm
+        /// </summary>
+        /// <param name="arguments"></param>
+        /// <returns>Returns greater common divisor</returns>
+        public static int GetGCDByEuclid(params int[] arguments)
+        {
+            DataVerification(arguments);
+            int gcd = GetGCDByEuclid(arguments[0], arguments[1]);
+            for (int i = 2; i < arguments.Length; i++)
+            {
+                gcd = GetGCDByEuclid(gcd, arguments[i]);
+            }
+
+            return gcd;
+        }
+        #endregion EuclidRealization
+        #region SteinRealization
+        /// <summary>
+        /// Finds and returns the greatest common divisor of 2 integers using the Stein algorithm
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <returns>Returns greater common divisor</returns>
+        public static int GetGCDByStein(int firstNumber, int secondNumber)
+        {
+            firstNumber = Math.Abs(firstNumber);
+            secondNumber = Math.Abs(secondNumber);
+            if (firstNumber == 0)
+            {
+                return secondNumber;
+            }
+
+            if (secondNumber == 0)
+            {
+                return firstNumber;
+            }
+
+            if (firstNumber == secondNumber)
+            {
+                return firstNumber;
+            }
+
+            if (firstNumber == 1 || secondNumber == 1)
+            {
+                return 1;
+            }
+
+            if (firstNumber % 2 == 0 && secondNumber % 2 == 0)
+            {
+                return 2 * GetGCDByStein(firstNumber / 2, secondNumber / 2);
+            }
+
+            if (firstNumber % 2 == 0)
+            {
+                return GetGCDByStein(firstNumber / 2, secondNumber);
+            }
+
+            if (secondNumber % 2 == 0)
+            {
+                return GetGCDByStein(firstNumber, secondNumber / 2);
+            }
+
+            if (firstNumber > secondNumber)
+            {
+                return GetGCDByStein((firstNumber - secondNumber) / 2, secondNumber);
+            }
+            else
+            {
+                return GetGCDByStein((secondNumber - firstNumber) / 2, firstNumber);
+            }
+        }
+
+        /// <summary>
+        /// Finds and returns the greatest common divisor of 3 integers using the Stein algorithm
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <param name="thirdNumber"></param>
+        /// <returns>Returns greater common divisor</returns>
+        public static int GetGCDByStein(int firstNumber, int secondNumber, int thirdNumber)
+        {
+           return GetGCDByStein(GetGCDByStein(firstNumber, secondNumber), thirdNumber);
+        }
+
+        /// <summary>
+        /// Finds and returns the greatest common divisor of n integers using the Stein algorithm
+        /// </summary>
+        /// <param name="arguments"></param>
+        /// <returns>Returns greater common divisor</returns>
+        public static int GetGCDByStein(params int[] arguments)
+        {
+            DataVerification(arguments);
+            int gcd = GetGCDByEuclid(arguments[0], arguments[1]);
+            for (int i = 2; i < arguments.Length; i++)
+            {
+                gcd = GetGCDByEuclid(gcd, arguments[i]);
+            }
+
+            return gcd;
+        }
+
+        /// <summary>
+        /// Validating arguments
+        /// </summary>
+        /// <param name="arguments"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArithmeticException"></exception>
+        private static void DataVerification(int[] arguments)
+        {            
+            if (arguments == null)
+            {
+               throw new ArgumentNullException();
+            }
+
+            if (arguments.Length < 2)
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+    #endregion SteinRealization
+}
