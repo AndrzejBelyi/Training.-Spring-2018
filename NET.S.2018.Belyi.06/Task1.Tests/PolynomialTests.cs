cd41cadd2e390ca10450d6030baaa24dfd021e01
Andrzej <Andrzej.Belyi@gmail.com>
2018-03-26 20:26:47 +0300

    Добавлен класс Plynomial(хромает на две ноги)+ тесты
    В данном классе переопределена операция сложения и метод Equals
    на этом пока всё,остальное позже,там почти аналогично.

---
+++ NET.S.2018.Belyi.06/Task1.Tests/PolynomialTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+namespace Task1.Tests
+{
+    public class PolynomialTests
+    {
+        [TestCase(1, 2, 3, 4, 5, 6, 7)]
+        public void PolynomialEquals_ValidParams_Equals(params int[] indexes)
+        {
+            Polynomial polynomial1 = new Polynomial(1,2,3,4,5,6,7);
+            Polynomial polynomial2 = new Polynomial(indexes);
+            Assert.AreEqual(polynomial1.Equals(polynomial2),true);
+        }
+        [TestCase(1, 2, 3, 4, 5, 6, 7)]
+        public void PolynomialEquals_ValidParams_NotEquals(params int[] indexes)
+        {
+            Polynomial polynomial1 = new Polynomial(1, 2, 3, 4, 5, 6);
+            Polynomial polynomial2 = new Polynomial(indexes);
+            Assert.AreEqual(polynomial1.Equals(polynomial2), false);
+        }
+        [Test]
+        public void PolynomialAdd_TwoPolynomial_ValidRes()
+        {
+            Polynomial polynomial1 = new Polynomial(1, 2, 3);
+            Polynomial polynomial2 = new Polynomial(3, 2, 1);
+            Polynomial expectedPolynomial = new Polynomial(4, 4, 4);
+            Assert.AreEqual(expectedPolynomial.Equals(polynomial1+polynomial2),true);
+        }
+    }
+}
---
+++ NET.S.2018.Belyi.06/Task1/Polynomial.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Polynomial
+    {
+        /// <summary>
+        /// Cooficients of polynomial
+        /// </summary>
+        private int[] indexes;
+        /// <summary>
+        /// Constructor of class
+        /// </summary>
+        /// <param name="indexes"></param>
+        public Polynomial(params int [] indexes)
+        {
+            this.indexes = indexes;
+        }
+        /// <summary>
+        /// Get length of indexes
+        /// </summary>
+        public int Length
+        {
+            get { return indexes.Length; }
+        }
+
+        /// <summary>
+        /// Addition two objects of Polynomial class
+        /// </summary>
+        /// <param name="firstPolynomial"></param>
+        /// <param name="secondPolynomial"></param>
+        /// <returns></returns>
+        public static Polynomial operator +(Polynomial firstPolynomial,Polynomial secondPolynomial)
+        {
+            int[] sum= new int[firstPolynomial.Length < secondPolynomial.Length ? firstPolynomial.Length : secondPolynomial.Length];
+            for (int i = 0; i < firstPolynomial.Length; i++)
+            {
+                sum[i] += firstPolynomial.indexes[i];
+            }
+            for (int i = 0; i < secondPolynomial.Length; i++)
+            {
+                sum[i] += secondPolynomial.indexes[i];
+            }
+            return new Polynomial(sum);
+        }
+
+        /// <summary>
+        /// Compare two objects of plynomial class and return true if are equal
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>True if two objects of Polynomial class are equal</returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            else
+            {
+                Polynomial poly = obj as Polynomial;
+                if(poly.Length!=this.Length)
+                {
+                    return false;
+                }
+                else
+                {
+                    for (int i = 0; i < poly.Length; i++)
+                    {
+                        if (poly.indexes[i] != this.indexes[i])
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        }
+}
