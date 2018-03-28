b7863df1fe7b4fa4fe6f10c5101734326bac4c32
Andrzej <Andrzej.Belyi@gmail.com>
2018-03-28 17:33:06 +0300

    Updated Polynomial

--- NET.S.2018.Belyi.06/Task1.Tests/PolynomialTests.cs
+++ NET.S.2018.Belyi.06/Task1.Tests/PolynomialTests.cs
@@ -8,27 +8,72 @@ namespace Task1.Tests
 {
     public class PolynomialTests
     {
-        [TestCase(1, 2, 3, 4, 5, 6, 7)]
-        public void PolynomialEquals_ValidParams_Equals(params int[] indexes)
-        {
-            Polynomial polynomial1 = new Polynomial(1,2,3,4,5,6,7);
-            Polynomial polynomial2 = new Polynomial(indexes);
-            Assert.AreEqual(polynomial1.Equals(polynomial2),true);
-        }
-        [TestCase(1, 2, 3, 4, 5, 6, 7)]
-        public void PolynomialEquals_ValidParams_NotEquals(params int[] indexes)
-        {
-            Polynomial polynomial1 = new Polynomial(1, 2, 3, 4, 5, 6);
-            Polynomial polynomial2 = new Polynomial(indexes);
-            Assert.AreEqual(polynomial1.Equals(polynomial2), false);
-        }
+        Polynomial poly =new Polynomial(1,2.2,3,4.11,5,6,7);
+
         [Test]
-        public void PolynomialAdd_TwoPolynomial_ValidRes()
+        public void Constructor_ArgumentException()
         {
-            Polynomial polynomial1 = new Polynomial(1, 2, 3);
-            Polynomial polynomial2 = new Polynomial(3, 2, 1);
-            Polynomial expectedPolynomial = new Polynomial(4, 4, 4);
-            Assert.AreEqual(expectedPolynomial.Equals(polynomial1+polynomial2),true);
+            Assert.Throws<ArgumentException>(() => new Polynomial());
         }
+
+        [Test]
+        public void Equals_ValidParams_Equals()
+        {
+            Polynomial polynomial1 = new Polynomial(1,2.2,3,4.11,5,6,7);
+            Assert.AreEqual(true, polynomial1.Equals(poly));
+        }
+
+        [Test]
+        public void OperatorEquals_ValidParams_Equals()
+        {
+            Polynomial polynomial1 = new Polynomial(1, 2.2, 3, 4.11, 5, 6, 7);
+            Assert.AreEqual(true, polynomial1==poly);
+        }
+
+        [Test]
+        public void OperatorNotEquals_ValidParams_NotEquals()
+        {
+            Polynomial polynomial1 = new Polynomial(1, 2, 3, 4.11, 5, 6, 7);
+            Assert.AreEqual(true, polynomial1 != poly);
+        }
+
+        [Test]
+        public void Equals_ValidParams_NotEquals()
+        {
+            Polynomial polynomial1 = new Polynomial(1, 2.2, 3, 4.11, 5, 5, 7);
+            Assert.AreEqual(false, polynomial1.Equals(poly));
+        }
+
+        [Test]
+        public void Equals_EqualsObjects_Equals()
+        {
+            Polynomial polynomial1 = poly;
+            Assert.AreEqual(true, poly.Equals(polynomial1));
+        }
+
+        [Test]
+        public void OperatorAddition_ValidData_ValidRes()
+        {
+            Polynomial polynomial1 = new Polynomial(1, 2.2, 3);
+            Polynomial polynomial2 = new Polynomial(3, 2.2 , 1);
+            Polynomial expectedPolynomial = new Polynomial(4, 4.4, 4);
+            Assert.AreEqual(true, expectedPolynomial.Equals(polynomial1 + polynomial2));
+        }
+
+        [Test]
+        public void OperatorSubstraction_ValidData_ValidRes()
+        {
+            Polynomial polynomial1 = new Polynomial(4, 4.4, 4);
+            Polynomial polynomial2 = new Polynomial(3, 2.2, 1);
+            Polynomial expectedPolynomial = new Polynomial(1, 2.2, 3);
+            Assert.AreEqual(true, expectedPolynomial.Equals(polynomial1 - polynomial2));
+        }
+
+        [Test]
+        public void ToString_ValidData_ValidRes()
+        {
+            Assert.AreEqual("(1)x^0 + (2,2)x^1 + (3)x^2 + (4,11)x^3 + (5)x^4 + (6)x^5 + (7)x^6", poly.ToString());
+        }
+
     }
 }
--- NET.S.2018.Belyi.06/Task1/Polynomial.cs
+++ NET.S.2018.Belyi.06/Task1/Polynomial.cs
@@ -1,31 +1,79 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Configuration;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Task1
 {
-    public class Polynomial
+    public sealed class Polynomial
     {
         /// <summary>
-        /// Cooficients of polynomial
+        /// Coefficients of polynomial
         /// </summary>
-        private int[] indexes;
+        private readonly double[] coefficients;
+        private static double eps;
+
         /// <summary>
-        /// Constructor of class
+        /// Constructor
         /// </summary>
-        /// <param name="indexes"></param>
-        public Polynomial(params int [] indexes)
+        /// <param name="coefficients">consists of coefficients of polynomial</param>
+        public Polynomial(params double[] coefficients)
         {
-            this.indexes = indexes;
+            if (coefficients.Length < 1)
+            {
+                throw new ArgumentException(nameof(coefficients));
+            }
+
+            this.coefficients = new double[coefficients.Length];
+            coefficients.CopyTo(this.coefficients, 0);
         }
+
         /// <summary>
-        /// Get length of indexes
+        /// Static constructor for setting the value of the variable "eps"
+        /// </summary>
+        static Polynomial()
+        {
+#pragma warning disable CS0618 // Type or member is obsolete
+            string str = ConfigurationSettings.AppSettings.Get("eps");
+#pragma warning restore CS0618 // Type or member is obsolete
+
+            try
+            {
+                eps = Double.Parse(str);
+            }
+            catch (Exception)
+            {
+                eps = 0.000001;
+            }
+        }
+
+        /// <summary>
+        /// Get length of coefficients
         /// </summary>
         public int Length
         {
-            get { return indexes.Length; }
+            get { return coefficients.Length; }
+        }
+
+        /// <summary>
+        /// Returns true if polynomials are equal,else returns false
+        /// </summary>
+        /// <param name="firstPolynomial"></param>
+        /// <param name="secondPolynomial"></param>
+        /// <returns>Returns the bool result of the comparison<returns>
+        public static bool operator ==(Polynomial firstPolynomial, Polynomial secondPolynomial)
+        {
+            return firstPolynomial.Equals(secondPolynomial);
+        }
+
+        /// <summary>
+        /// Returns true if polynomials are not equal,else returns false
+        /// </summary>
+        /// <param name="firstPolynomial"></param>
+        /// <param name="secondPolynomial"></param>
+        /// <returns>Returns the bool result of the comparison<returns>
+        public static bool operator !=(Polynomial firstPolynomial, Polynomial secondPolynomial)
+        {
+            return !firstPolynomial.Equals(secondPolynomial);
         }
 
         /// <summary>
@@ -33,23 +81,93 @@ namespace Task1
         /// </summary>
         /// <param name="firstPolynomial"></param>
         /// <param name="secondPolynomial"></param>
-        /// <returns></returns>
-        public static Polynomial operator +(Polynomial firstPolynomial,Polynomial secondPolynomial)
+        /// <returns>Returns the result of addition of two polynomials</returns>
+        public static Polynomial operator +(Polynomial firstPolynomial, Polynomial secondPolynomial)
         {
-            int[] sum= new int[firstPolynomial.Length < secondPolynomial.Length ? firstPolynomial.Length : secondPolynomial.Length];
-            for (int i = 0; i < firstPolynomial.Length; i++)
+            if (firstPolynomial == null)
             {
-                sum[i] += firstPolynomial.indexes[i];
+                throw new ArgumentNullException(nameof(firstPolynomial));
             }
+
+            if (secondPolynomial == null)
+            {
+                throw new ArgumentNullException(nameof(secondPolynomial));
+            }
+
+            double[] sum = new double[firstPolynomial.Length < secondPolynomial.Length ? firstPolynomial.Length : secondPolynomial.Length];
+            Array.Copy(firstPolynomial.coefficients, sum, firstPolynomial.coefficients.Length);
             for (int i = 0; i < secondPolynomial.Length; i++)
             {
-                sum[i] += secondPolynomial.indexes[i];
+                sum[i] += secondPolynomial.coefficients[i];
             }
+
             return new Polynomial(sum);
         }
 
         /// <summary>
-        /// Compare two objects of plynomial class and return true if are equal
+        /// Returns opposed Polynomial
+        /// </summary>
+        /// <param name="poly">Polynomial to opposite</param>
+        /// <returns>Returns a polynomial with opposite coefficientes<returns>
+        public static Polynomial operator -(Polynomial poly)
+        {
+            if (poly == null)
+            {
+                throw new ArgumentNullException(nameof(poly));
+            }
+
+            double[] sum = new double[poly.Length];      
+            for (int i = 0; i < poly.Length; i++)
+            {
+                sum[i] = -poly.coefficients[i];
+            }
+
+            return new Polynomial(sum);
+        }
+
+        /// <summary>
+        /// Subtraction of two Polynomial
+        /// </summary>
+        /// <param name="firstPolynomial"></param>
+        /// <param name="secondPolynomial"></param>
+        /// <returns>returns Polynomial<returns>
+        public static Polynomial operator -(Polynomial firstPolynomial, Polynomial secondPolynomial)
+        {           
+            return firstPolynomial + (-secondPolynomial);
+        }
+
+        /// <summary>
+        /// Subtraction of  two Polynomial
+        /// </summary>
+        /// <param name="firstPolynomial"></param>
+        /// <param name="secondPolynomial"></param>
+        /// <returns>Returns the result of substraction of two polynomials<returns>
+        public static Polynomial operator *(Polynomial firstPolynomial, Polynomial secondPolynomial)
+        {
+            if (firstPolynomial == null)
+            {
+                throw new ArgumentNullException(nameof(firstPolynomial));
+            }
+
+            if (secondPolynomial == null)
+            {
+                throw new ArgumentNullException(nameof(secondPolynomial));
+            }
+
+            double[] result = new double[firstPolynomial.Length + secondPolynomial.Length];
+            for (int i = 0; i < firstPolynomial.coefficients.Length; i++)
+            {
+                for (int j = 0; j < secondPolynomial.coefficients.Length; j++)
+                {
+                    result[i + j] += firstPolynomial.coefficients[i] * secondPolynomial.coefficients[j];
+                }
+            }
+
+            return new Polynomial(result);
+        }
+
+        /// <summary>
+        /// Compares instance of polynomial class and object and return true if are equal
         /// </summary>
         /// <param name="obj"></param>
         /// <returns>True if two objects of Polynomial class are equal</returns>
@@ -61,24 +179,80 @@ namespace Task1
             }
             else
             {
-                Polynomial poly = obj as Polynomial;
-                if(poly.Length!=this.Length)
+                if (ReferenceEquals(this, obj))
                 {
-                    return false;
+                    return true;
                 }
                 else
                 {
-                    for (int i = 0; i < poly.Length; i++)
+                    return this.Equals((Polynomial)obj);
+                }
+            }            
+        }
+
+        /// <summary>
+        /// Compares instance of polynomial class and object and return true if are equal
+        /// </summary>
+        /// <param name="poly"></param>
+        /// <returns>True if two objects of Polynomial class are equal</returns>
+        public bool Equals(Polynomial poly)
+        {
+            if (ReferenceEquals(poly, null))
+            {
+                return false;
+            }
+            else
+            {
+                if (ReferenceEquals(this, poly))
+                {
+                    return true;
+                }
+                else
+                {
+                    if (poly.Length != this.Length)
                     {
-                        if (poly.indexes[i] != this.indexes[i])
+                        return false;
+                    }
+                    else
+                    {
+                        for (int i = 0; i < poly.Length; i++)
                         {
-                            return false;
+                            if (Math.Abs(poly.coefficients[i] - this.coefficients[i]) > eps)
+                            {
+                                return false;
+                            }
                         }
                     }
                 }
             }
+
             return true;
         }
 
+        /// <summary>
+        /// Return string representation of the polynomial
+        /// </summary>
+        /// <returns>string representation of the polynomial</returns>
+        public override string ToString()
+        {
+            StringBuilder strRepresentation = new StringBuilder();
+            for (int i = 0; i < coefficients.Length; i++)
+            {
+                strRepresentation.Append($"({coefficients[i]})x^{i} + ");
+            }
+
+            strRepresentation.Remove(strRepresentation.Length - 3, 3);
+            return strRepresentation.ToString();
         }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>       
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return coefficients.GetHashCode();
+        }
+    }
 }
