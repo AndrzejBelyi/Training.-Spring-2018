5a65f0c194ea17ff78ba1d4231cf7ffa578cb797
Andrzej <Andrzej.Belyi@gmail.com>
2018-04-01 14:12:04 +0300

    Задание 1(c помощью делегата Predicate<>)

---
+++ NET.S.2018.Belyi.09/Task1.Tests/FiltersTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Task1.Tests
+{
+    public class FiltersTests
+    {
+       public Predicate<int> isPositive = (x) => x >= 0;
+       public Predicate<int> isNegative = (x) => x <= 0;
+        /// <summary>
+        /// This is a method for verifying the algorithm for filtering an array on valid data
+        /// </summary>
+        /// <param name="actualArray"></param>
+        /// <param name="expectedArray"></param>
+        /// <param name="targetDigit"></param>
+        [Test]
+        [TestCase(new int[] { -7, -1, 2, -3, 4, -5, 6, -7, 68, 69, 70, 15, 17 }, new int[] { -7, -1, -3, -5, -7})]
+        [TestCase(new int[] { 7, -7, 70, 17 }, new int[] { -7 })]
+        public void FilterDigit_UnfilteredArray_FilteredArray(int[] actualArray, int[] expectedArray)
+        {
+            actualArray = Filters.FilterDigit(actualArray,isNegative);
+            CollectionAssert.AreEqual(expectedArray, actualArray);
+        }
+
+        /// <summary>
+        /// This is a method for verifying the algorithm for filtering an array on valid data
+        /// </summary>
+        /// <param name="actualArray"></param>
+        /// <param name="expectedArray"></param>
+        /// <param name="targetDigit"></param>
+        [Test]
+        [TestCase(new int[] { -7, -1, 2, -3, 4, -5, 6, -7, 68, 69, 70, 15, 17 }, new int[] { 2, 4, 6, 68, 69, 70, 15, 17})]
+        public void FilterDigit1_UnfilteredArray_FilteredArray(int[] actualArray, int[] expectedArray)
+        {
+            actualArray = Filters.FilterDigit(actualArray, isPositive);
+            CollectionAssert.AreEqual(expectedArray, actualArray);
+        }
+
+        /// <summary>
+        /// This is a method for verifying the algorithm for filtering an array on invalid data(null reference)
+        /// </summary>
+        [Test]
+        public void FilterDigit_ArrayNullReference_ArgumentNullException()
+        {
+           Assert.That(() => Filters.FilterDigit(null, isNegative), Throws.TypeOf<ArgumentNullException>());
+        }
+
+        /// <summary>
+        /// This is a method for verifying the algorithm for filtering an array on invalid data(null reference)
+        /// </summary>
+        [Test]
+        public void FilterDigit_ArrayNullReference2_ArgumentNullException()
+        {
+            Assert.That(() => Filters.FilterDigit(new int[] { 1, 2 }, null), Throws.TypeOf<ArgumentNullException>());
+        }
+    }
+}
---
+++ NET.S.2018.Belyi.09/Task1/Filters.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Filters
+    {
+        /// <summary>
+        /// This method returns an array of numbers
+        /// </summary>
+        /// <param name="array">Array for filtration</param>
+        /// <returns>Filtered array</returns>   
+        public static int[] FilterDigit(int[] array, Predicate<int> predict)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (predict == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            List<int> filtredArray = new List<int>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (predict(array[i]))
+                {
+                    filtredArray.Add(array[i]);
+                }
+            }
+
+            return filtredArray.ToArray();
+        }
+    }
+}
