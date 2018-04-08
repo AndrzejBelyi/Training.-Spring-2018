7bfb37c5c58f01594e374cabc9040c9a2072caa1
Andrzej <Andrzej.Belyi@gmail.com>
2018-04-08 18:08:00 +0300

    Добавлено задание 1+ тесты(работает,но мне не нравится)

---
+++ NET.S.2018.Belyi.12/Task1.Tests/GCDTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Task1;
+
+namespace Task1.Tests
+{
+    public class GCDTests
+    {
+        public AlgorithmGcdDelegate algorithm = GCD.GetGCDByEuclid;
+
+        [Test]
+        [TestCase(9, -3, 3)]
+        [TestCase(10000, 15000, 5000)]
+        [TestCase(10, 10, 10)]
+        [TestCase(10, 9, 1)]
+        [TestCase(-3, 9, 3)]
+        [TestCase(15000, 10000, 5000)]
+        [TestCase(10, 10, 10)]
+        [TestCase(9, 10, 1)]
+        public void GetGcdByEuclid_TwoArguments(int firstNumber, int secondNumber, int expectedResult)
+        {
+           
+            int actualResult = GCD.GetGCD(algorithm, firstNumber, secondNumber);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        [TestCase(-9, -18, 27, 9)]
+        [TestCase(10, 10, 9, 1)]
+        [TestCase(3, 3, 3, 3)]
+        [TestCase(0, 3, 6, 3)]
+        [TestCase(0, 0, 3, 3)]
+        public void GetGcdByEuclid_ThreeArguments(int firstNumber, int secondNumber, int thirdNumber, int expectedResult)
+        {
+            int actualResult = GCD.GetGCD(algorithm, firstNumber, secondNumber, thirdNumber);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        [TestCase(9, -18, 27, -36, 9)]
+        [TestCase(1, 9, 7, 5, 3, 11)]
+        [TestCase(3, 3, 3, 3, 3, 3, 3, 3)]
+        [TestCase(3, 3, 6, 9, 0)]
+        [TestCase(3, 0, 6, 3, 0)]
+        public void GetGcdByEuclid_ManyArguments(int expectedResult, params int[] arguments)
+        {
+            int actualResult = GCD.GetGCD(algorithm, arguments);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+        public AlgorithmGcdDelegate algorithm2 = GCD.GetGCDByEuclid;
+        [Test]
+        [TestCase(9, -3, 3)]
+        [TestCase(10000, 15000, 5000)]
+        [TestCase(10, 10, 10)]
+        [TestCase(10, 9, 1)]
+        [TestCase(-3, 9, 3)]
+        [TestCase(15000, 10000, 5000)]
+        [TestCase(10, 10, 10)]
+        [TestCase(9, 10, 1)]
+        public void GetGcdByStein_TwoArguments(int firstNumber, int secondNumber, int expectedResult)
+        {
+            int actualResult = GCD.GetGCD(algorithm, firstNumber, secondNumber);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        [TestCase(-9, -18, 27, 9)]
+        [TestCase(10, 10, 9, 1)]
+        [TestCase(3, 3, 3, 3)]
+        [TestCase(0, 3, 6, 3)]
+        public void GetGcdByStein_ThreeArguments(int firstNumber, int secondNumber, int thirdNumber, int expectedResult)
+        {
+            int actualResult = GCD.GetGCD(algorithm ,firstNumber, secondNumber, thirdNumber);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        [TestCase(9, -18, 27, -36, 9)]
+        [TestCase(1, 9, 7, 5, 3, 11)]
+        [TestCase(3, 3, 3, 3, 3, 3, 3, 3)]
+        [TestCase(3, 3, 6, 9, 0)]
+        [TestCase(3, 0, 6, 3, 0)]
+
+        public void GetGcdByStein_ManyArguments(int expectedResult, params int[] arguments)
+        {
+            int actualResult = GCD.GetGCD(algorithm , arguments);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
---
+++ NET.S.2018.Belyi.12/Task1/GCD.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public delegate int AlgorithmGcdDelegate(int value1, int value2);
+
+    public static class GCD
+    {    
+
+        public static int GetGCD(AlgorithmGcdDelegate algorithmGcd, int firstNumber, int secondNumber)
+        {
+            return algorithmGcd(firstNumber, secondNumber);
+        }
+
+        public static int GetGCD(AlgorithmGcdDelegate algorithmGcd, int firstNumber, int secondNumber, int thirdNumber)
+        {
+            return algorithmGcd(algorithmGcd(firstNumber, secondNumber), thirdNumber);
+        }
+
+        public static int GetGCD(AlgorithmGcdDelegate algorithmGcd, params int[] arguments)
+        {
+            DataVerification(arguments);
+            int gcd = algorithmGcd(arguments[0], arguments[1]);
+            for (int i = 2; i < arguments.Length; i++)
+            {
+                gcd = algorithmGcd(gcd, arguments[i]);
+            }
+
+            return gcd;
+        }
+
+        public static int GetGCDByStein(int firstNumber, int secondNumber)
+        {
+            firstNumber = Math.Abs(firstNumber);
+            secondNumber = Math.Abs(secondNumber);
+            if (firstNumber == 0)
+            {
+                return secondNumber;
+            }
+
+            if (secondNumber == 0)
+            {
+                return firstNumber;
+            }
+
+            if (firstNumber == secondNumber)
+            {
+                return firstNumber;
+            }
+
+            if (firstNumber == 1 || secondNumber == 1)
+            {
+                return 1;
+            }
+
+            if (firstNumber % 2 == 0 && secondNumber % 2 == 0)
+            {
+                return 2 * GetGCDByStein(firstNumber / 2, secondNumber / 2);
+            }
+
+            if (firstNumber % 2 == 0)
+            {
+                return GetGCDByStein(firstNumber / 2, secondNumber);
+            }
+
+            if (secondNumber % 2 == 0)
+            {
+                return GetGCDByStein(firstNumber, secondNumber / 2);
+            }
+
+            if (firstNumber > secondNumber)
+            {
+                return GetGCDByStein((firstNumber - secondNumber) / 2, secondNumber);
+            }
+            else
+            {
+                return GetGCDByStein((secondNumber - firstNumber) / 2, firstNumber);
+            }
+        }
+
+
+        public static int GetGCDByEuclid(int firstNumber, int secondNumber)
+        {
+            firstNumber = Math.Abs(firstNumber);
+            secondNumber = Math.Abs(secondNumber);
+            if (firstNumber == 0)
+            {
+                return secondNumber;
+            }
+
+            if (secondNumber == 0)
+            {
+                return firstNumber;
+            }
+
+            if (firstNumber == secondNumber)
+            {
+                return firstNumber;
+            }
+
+            if (firstNumber == 1 || secondNumber == 1)
+            {
+                return 1;
+            }
+
+            return GetGCDByEuclid(secondNumber, firstNumber % secondNumber);
+        }
+
+        private static void DataVerification(int[] arguments)
+        {
+            if (arguments == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (arguments.Length < 2)
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+}
