f50bff475d272d8fde39836ca1b8a4373d03ddb1
Andrzej <Andrzej.Belyi@gmail.com>
2018-04-10 19:09:23 +0300

    Частично task2

---
+++ NET.S.2018.Belyi.12/Task2/Sorts.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Sorts
+    {
+        /// <summary>
+        /// Bubbles the sort.
+        /// </summary>
+        /// <param name="array">The array.</param>
+        /// <param name="comparer">The comparer.</param>
+        /// <exception cref="ArgumentNullException">
+        /// array
+        /// or
+        /// comparer
+        /// </exception>
+        public static void BubbleSort(int[][] array, Func<int[], int[], int> comparer)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            if (comparer == null)
+            {
+                throw new ArgumentNullException(nameof(comparer));
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (comparer(array[j], array[j + 1]) > 0)
+                    {
+                        Swap(ref array[j], ref array[j + 1]);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Bubbles the sort.
+        /// </summary>
+        /// <param name="array">The array.</param>
+        /// <param name="comparer">The comparer.</param>
+        /// <exception cref="ArgumentNullException">
+        /// array
+        /// or
+        /// comparer
+        /// </exception>
+        public static void BubbleSort(int[][] array, IComparer<int[]> comparer)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            if (comparer == null)
+            {
+                throw new ArgumentNullException(nameof(comparer));
+            }
+
+            Func<int[], int[], int> comparerDelegate = comparer.Compare;
+            BubbleSort(array,comparerDelegate);
+        }
+        /// <summary>
+        /// Swap two arrays
+        /// </summary>
+        /// <param name="array1"></param>
+        /// <param name="array2"></param>
+        private static void Swap(ref int[] array1, ref int[] array2)
+        {
+            int[] temp = array1;
+            array1 = array2;
+            array2 = temp;
+        }
+    }
+}
+}
