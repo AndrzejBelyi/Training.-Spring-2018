4ce8bb8b6d6300f4c01feb2ed7a296f7e1c17968
Andrzej <Andrzej.Belyi@gmail.com>
2018-04-12 17:50:25 +0300

    Задание 3(алгоритм с возможностью переполнения памяти и пр.)

---
+++ NET.S.2018.Belyi.14/Task3.Tests/GenerateFibonacciTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3.Tests
+{
+    public class GenerateFibonacciTests
+    {
+        [Test]
+        [TestCase(2, new int[] { 0, 1})]
+        [TestCase(8, new int[] { 0, 1, 1, 2, 3, 5, 8, 13})]
+        [TestCase(13, new int[] {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 })]
+        public void GenerateFibonacci_Test(int n,int[] expected)
+        {
+            CollectionAssert.AreEqual(expected, GenerateFibonacci.Generate(n));
+        }
+
+        [Test]
+        public void GenerateFibonacci_Fail()
+        {
+            Assert.Throws<ArgumentException>(() => GenerateFibonacci.Generate(-1));
+        }
+    }
+}
---
+++ NET.S.2018.Belyi.14/Task3/GenerateFibonacci.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public static class GenerateFibonacci
+    {
+        /// <summary>
+        /// Generates the specified n.
+        /// </summary>
+        /// <param name="n">The n.</param>
+        /// <returns></returns>
+        /// <exception cref="System.ArgumentException">n</exception>
+        public static int[] Generate(int n)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentException(nameof(n));
+            }
+
+            int[] array = new int[n];
+            array[0] = 0;
+            array[1] = 1;
+            for (int i = 2; i < n; i++)
+            {
+                array[i] = unchecked(array[i - 1] + array[i - 2]);
+            }
+
+            return array;
+        }
+    }
+}
