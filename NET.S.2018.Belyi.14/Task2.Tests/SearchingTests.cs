77072ce70dd7ef51085d9475687c145ee7af8462
Andrzej <Andrzej.Belyi@gmail.com>
2018-04-12 16:43:34 +0300

    Пока существует проблема с массивом отсортированном по убыванию.

---
+++ NET.S.2018.Belyi.14/Task2.Tests/SearchingTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Task2.Tests
+{   public class Cat : IComparable, IComparable<Cat>
+    {
+       public int mass;
+       public string name;
+
+        public Cat(int mass,string name)
+        {
+            this.mass = mass;
+            this.name = name;
+        }
+
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CompareTo(Cat other)
+        {
+            return this.mass - other.mass;
+        }
+    }
+
+    public class CustomCatsComparer : IComparer<Cat>
+    {
+        public int Compare(Cat x, Cat y)
+        {
+            return x.name.Length - y.name.Length;
+        }
+    }
+
+    public class SearchingTests
+    {
+        [Test]
+        [TestCase(7, new int[] { 1, 3, 5, 7, 9 }, 3)]
+        [TestCase(7, new int[] { 1, 3, 5, 7, 7, 7}, 3)]
+        public void BinarySearching_DefaultCompare(int f , int[] array,int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, Searching.BinarySearch(f, array));
+        }
+
+        [Test]
+        public void BinarySearching_CustomIComparable()
+        {
+           Cat[] cats = new Cat[] { new Cat(1, "Кыса"), new Cat(3, "Кыса2"), new Cat(2, "Искомая") };
+            Assert.AreEqual(1, Searching.BinarySearch(new Cat(3,"Кыcа4"), cats));
+        }
+
+        [Test]
+        public void BinarySearching_CustomComparer()
+        {
+            Cat[] cats = new Cat[] { new Cat(1, "Кыса"), new Cat(1, "Кот"), new Cat(2, "Искомая") };
+            Assert.AreEqual(2, Searching.BinarySearch(new Cat(6,"Искомая"), cats, new CustomCatsComparer()));
+        }
+
+
+        [Test]
+        public void BinarySearching_ArgumentNullException1()
+        {
+            Cat nullcat = null;
+            Cat[] cats = new Cat[] { new Cat(1, "Кошка"), new Cat(1, "Кот"), new Cat(2, "Искомая") };
+            Assert.Throws<ArgumentNullException>(() => Searching.BinarySearch(nullcat,cats));
+        }
+
+        [Test]
+        public void BinarySearching_ArgumentNullException2()
+        {
+            Cat cat1 = new Cat(1, "www");
+            Cat[] cats = null;
+            Assert.Throws<ArgumentNullException>(() => Searching.BinarySearch(cat1, cats));
+        }
+
+        [Test]
+        public void BinarySearching_ArgumentNullException3()
+        {
+            CustomCatsComparer customcomparer = null;
+            Cat cat1 = new Cat(1, "www");
+            Cat[] cats = new Cat[] { new Cat(1, "Кошка"), new Cat(1, "Кот"), new Cat(2, "Искомая") };
+            Assert.Throws<ArgumentNullException>(() => Searching.BinarySearch(cat1, cats,customcomparer));
+        }
+    }
+}
---
+++ NET.S.2018.Belyi.14/Task2/Searching.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public static class Searching
+    {
+        /// <summary>
+        /// Tries the binary search.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="item">The item.</param>
+        /// <param name="collection">The collection.</param>
+        /// <param name="index">The index.</param>
+        /// <returns></returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// item
+        /// or
+        /// collection
+        /// </exception>
+        public static int BinarySearch<T>(T item, IEnumerable<T> collection)
+        where T: IComparable<T>
+        {
+            CheckInput(item, collection);
+
+            int index;
+            if (TryBinarySearch(item, collection, Comparer<T>.Default, out index))
+            {
+                return index;
+            }
+
+            return -1;
+        }
+
+
+        /// <summary>
+        /// Binaries the search.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="item">The item.</param>
+        /// <param name="collection">The collection.</param>
+        /// <param name="comparer">The comparer.</param>
+        /// <returns></returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// item
+        /// or
+        /// collection
+        /// or
+        /// item
+        /// </exception>
+        public static int BinarySearch<T>(T item, IEnumerable<T> collection, IComparer<T> comparer)
+        {
+            CheckInput(item, collection);
+            if (ReferenceEquals(comparer, null))
+            {
+                throw new ArgumentNullException($"{nameof(item)} is null!");
+            }
+
+            int index;
+            if (TryBinarySearch(item, collection, comparer, out index))
+            {
+                return index;
+            }
+
+            return -1;
+        }
+
+
+        /// <summary>
+        /// Binaries the search.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="item">The item.</param>
+        /// <param name="collection">The collection.</param>
+        /// <param name="comparison">The comparison.</param>
+        /// <returns></returns>
+        /// <exception cref="System.ArgumentNullException">item</exception>
+        public static int BinarySearch<T>(T item, IEnumerable<T> collection, Comparison<T> comparison)
+        {
+            CheckInput(item, collection);
+            if (ReferenceEquals(comparison, null))
+            {
+                throw new ArgumentNullException($"{nameof(item)} is null!");
+            }
+
+            int index;
+            if (TryBinarySearch(item, collection, Comparer<T>.Create(comparison), out index))
+            {
+                return index;
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Tries the binary search.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="item">The item.</param>
+        /// <param name="collection">The collection.</param>
+        /// <param name="comparer">The comparer.</param>
+        /// <param name="index">The index.</param>
+        /// <returns></returns>
+        private static bool TryBinarySearch<T>(T item, IEnumerable<T> collection, IComparer<T> comparer, out int index)
+        {
+            index = 0;
+            int left = 0;
+            int right = collection.Count();
+            int mid = 0;
+
+            while (!(left >= right))
+            {
+                mid = left + (right - left) / 2;
+
+                if (comparer.Compare(collection.ElementAt(mid), item) == 0)
+                {
+                    index = mid;
+                    return true;
+                }
+
+                if (comparer.Compare(collection.ElementAt(mid), item) > 0)
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Checks the input.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="item">The item.</param>
+        /// <param name="collection">The collection.</param>
+        /// <exception cref="System.ArgumentNullException">
+        /// item
+        /// or
+        /// collection
+        /// </exception>
+        private static void CheckInput<T>(T item, IEnumerable<T> collection)
+        {
+            if (ReferenceEquals(item, null))
+            {
+                throw new ArgumentNullException($"{nameof(item)} is null!");
+            }
+
+            if (ReferenceEquals(collection, null))
+            {
+                throw new ArgumentNullException($"{nameof(collection)} is null!");
+            }
+        }
+    } 
+}
