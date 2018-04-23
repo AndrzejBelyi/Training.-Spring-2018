9af6e1d23443ce4e92fc104e0be8e7e621dd6864
Andrzej <Andrzej.Belyi@gmail.com>
2018-04-23 10:01:21 +0300

    task1

---
+++ NET.W.2017.LastName.Test/Task1.Solution/PasswordCheckerService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Task1
+{
+    public class PasswordCheckerService
+    {
+        private IPasswordRepository repository = new SqlRepository();
+
+        private IPasswordChecker checker = new CheckPassword();
+
+        public PasswordCheckerService()
+        {
+        }
+
+        public PasswordCheckerService(IPasswordRepository repository, IPasswordChecker checker)
+        {
+            this.checker = checker;
+            this.repository = repository;
+        }
+
+        public Tuple<bool, string> VerifyPassword(string password)
+        {
+            Tuple<bool,string> tuple = checker.VerifyPassword(password);
+            
+            repository.Create(password);
+
+            return tuple;           
+        }
+    }
+}
