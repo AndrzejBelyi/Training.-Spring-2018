a41616530777bbcb6a5ebaae02c601bd5f275405
Andrzej <Andrzej.Belyi@gmail.com>
2018-04-23 11:43:47 +0300

    упс

---
+++ NET.W.2017.LastName.Test/Task2.Solution/RandomBytesFileGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace Task_2
+{
+    public class RandomBytesFileGenerator : RandomFileGenerator
+    {
+        public override string WorkingDirectory
+        {
+            get
+            {
+                return "Files with random bytes";
+            }
+        }
+
+        public override string FileExtension
+        {
+            get
+            {
+                return ".bytes";
+            }
+        }
+
+        protected override byte[] GenerateFileContent(int contentLength)
+        {
+            var random = new Random();
+
+            var fileContent = new byte[contentLength];
+
+            random.NextBytes(fileContent);
+
+            return fileContent;
+        }
+
+        private byte[] GenerateFileContent(int contentLength)
+        {
+            var random = new Random();
+
+            var fileContent = new byte[contentLength];
+
+            random.NextBytes(fileContent);
+
+            return fileContent;
+        }
+    }
+}
